f74bb1e10ea763548ed69b628799a3bb7f159546
aditya <adubombatkar@gmail.com>
2025-08-19 22:02:52 +0530

    Intial commit - origin

---
+++ greeting-micro-service/src/main/java/dev/example/microdemo/GreetingApplication.java
@@ -0,0 +1,15 @@
+package dev.example.microdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+}
